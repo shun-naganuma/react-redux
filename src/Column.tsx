f050e3266f5118cf882906c70502d9fdc1dbeb94
shun-naganuma <129668177+shun-naganuma@users.noreply.github.com>
2024-09-06 16:45:09 +0900

    4-3でエラー発生中（画面がずっとLoadingになっている）

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,37 +6,48 @@ import { PlusIcon } from './icon'
 import { InputForm as _InputForm } from './InputForm'
 
 export function Column({
-  filterValue: rawFilterValue,
   title,
+  filterValue: rawFilterValue,
   cards: rawCards,
   onCardDragStart,
   onCardDrop,
   onCardDeleteClick,
+  text,
+  onTextChange,
+  onTextConfirm,
+  onTextCancel,
 }: {
   title?: string
   filterValue?: string
-  cards: {
+  cards?: {
     id: string
     text?: string
   }[]
   onCardDragStart?(id: string): void
   onCardDrop?(entered: string | null): void
   onCardDeleteClick?(id: string): void
+  text?: string
+  onTextChange?(value: string): void
+  onTextConfirm?(): void
+  onTextCancel?(): void
 }) {
   const filterValue = rawFilterValue?.trim()
   const keywords = filterValue?.toLowerCase().split(/\s+/g) ?? []
-  const cards = rawCards.filter(({ text }) =>
+  const cards = rawCards?.filter(({ text }) =>
     keywords?.every(w => text?.toLowerCase().includes(w)),
   )
 
-  const totalCount = rawCards.length
-
-  const [text, setText] = useState('')
+  const totalCount = rawCards?.length ?? -1
 
   const [inputMode, setInputMode] = useState(false)
   const toggleInput = () => setInputMode(v => !v)
-  const confirmInput = () => setText('')
-  const cancelInput = () => setInputMode(false)
+  const confirmInput = () => {
+    onTextConfirm?.()
+  }
+  const cancelInput = () => {
+    setInputMode(false)
+    onTextCancel?.()
+  }
 
   const [draggingCardID, setDraggingCardID] = useState<string | undefined>(
     undefined,
@@ -49,7 +60,7 @@ export function Column({
   return (
     <Container>
       <Header>
-        <CountBadge>{totalCount}</CountBadge>
+        {totalCount >= 0 && <CountBadge>{totalCount}</CountBadge>}
         <ColumnName>{title}</ColumnName>
 
         <AddButton onClick={toggleInput} />
@@ -58,42 +69,48 @@ export function Column({
       {inputMode && (
         <InputForm
           value={text}
-          onChange={setText}
+          onChange={onTextChange}
           onConfirm={confirmInput}
           onCancel={cancelInput}
         />
       )}
 
-      {filterValue && <ResultCount>{cards.length} results</ResultCount>}
+      {!cards ? (
+        <Loading />
+      ) : (
+        <>
+          {filterValue && <ResultCount>{cards.length} results</ResultCount>}
 
-      <VerticalScroll>
-        {cards.map(({ id, text }, i) => (
-          <Card.DropArea
-            key={id}
-            disabled={
-              draggingCardID !== undefined &&
-              (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
-            }
-            onDrop={() => onCardDrop?.(id)}
-          >
-            <Card
-              text={text}
-              onDragStart={() => handleCardDragStart(id)}
-              onDragEnd={() => setDraggingCardID(undefined)}
-              onDeleteClick={() => onCardDeleteClick?.(id)}
+          <VerticalScroll>
+            {cards.map(({ id, text }, i) => (
+              <Card.DropArea
+                key={id}
+                disabled={
+                  draggingCardID !== undefined &&
+                  (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
+                }
+                onDrop={() => onCardDrop?.(id)}
+              >
+                <Card
+                  text={text}
+                  onDragStart={() => handleCardDragStart(id)}
+                  onDragEnd={() => setDraggingCardID(undefined)}
+                  onDeleteClick={() => onCardDeleteClick?.(id)}
+                />
+              </Card.DropArea>
+            ))}
+
+            <Card.DropArea
+              style={{ height: '100%' }}
+              disabled={
+                draggingCardID !== undefined &&
+                cards[cards.length - 1]?.id === draggingCardID
+              }
+              onDrop={() => onCardDrop?.(null)}
             />
-          </Card.DropArea>
-        ))}
-
-        <Card.DropArea
-          style={{ height: '100%' }}
-          disabled={
-            draggingCardID !== undefined &&
-            cards[cards.length - 1]?.id === draggingCardID
-          }
-          onDrop={() => onCardDrop?.(null)}
-        />
-      </VerticalScroll>
+          </VerticalScroll>
+        </>
+      )}
     </Container>
   )
 }
@@ -149,6 +166,13 @@ const AddButton = styled.button.attrs({
 
 const InputForm = styled(_InputForm)`
   padding: 8px;
+`
+
+const Loading = styled.div.attrs({
+  children: 'Loading...',
+})`
+  padding: 8px;
+  font-size: 14px;
 `
 
 const ResultCount = styled.div`
---
+++ src/util.ts
@@ -0,0 +1,42 @@
+/**
+ * ランダムな ID `[0-9A-Za-z_-]{12}` を作成する
+ */
+export function randomID() {
+  const alphabet =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-'
+
+  let id = ''
+  for (let i = 12; i > 0; i--) {
+    id += alphabet[(Math.random() * 64) | 0]
+  }
+
+  return id
+}
+
+/**
+ * リストをリストの順序情報に従ってソートした新しいリストを返す
+ *
+ * @param list リスト
+ * @param order リストの順序情報
+ * @param head リストの先頭のキー
+ */
+export function sortBy<
+  E extends { id: Exclude<V, null> },
+  V extends string | null,
+>(list: E[], order: Record<string, V>, head: Exclude<V, null>) {
+  const map = list.reduce((m, e) => m.set(e.id, e), new Map<V, E>())
+
+  const sorted: typeof list = []
+
+  let id = order[head]
+  for (let i = list.length; i > 0; i--) {
+    if (!id || id === head) break
+
+    const e = map.get(id)
+    if (e) sorted.push(e)
+
+    id = order[id as Exclude<V, null>]
+  }
+
+  return sorted
+}
